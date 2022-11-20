74ee144f39deb0ce1531eb103b3c5cc823b97669
DanielleDaly <daniellerdaly@gmail.com>
2022-11-20 21:33:19 +0000

    Add event listeners to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,30 @@
+let playerScore = document.getElementById("player-score-number");
+let computerScore = document.getElementById("computer-score-number");
+let winnerMessage = document.getElementById("winner-message");
+let winnerExplanation = document.getElementById("winner-explanation");
+let winner = document.getElementById("winner");
+let playerChoice = document.getElementById("your-choice");
+let computerChoice = document.getElementById("computer-choice");
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("id") === "reset-button") {
+                alert("You clicked Play Again!");
+            } else {
+                let option = this.getAttribute("id");
+                alert(`You clicked ${option}`);
+            }
+        });
+    }
+});
+
 function playGame() {}
 function showChoices() {}
 function checkWinner() {}
-function winnerMessage() {}
+function setWinnerMessage() {}
 function incrementScorePlayer() {}
 function incrementScoreComputer() {}
-function winner() {}
+function setWinner() {}
