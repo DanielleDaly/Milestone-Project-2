24b8ec082c45d3eee417367f8b19acd42c0cb67e
DanielleDaly <daniellerdaly@gmail.com>
2022-11-21 21:41:31 +0000

    Set images to display player and computer choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ const computerScore = document.getElementById("computer-score-number");
 const winnerMessage = document.getElementById("winner-message");
 const winnerExplanation = document.getElementById("winner-explanation");
 const winner = document.getElementById("winner");
-const playerChoiceImage = document.getElementById("your-choice");
-const computerChoiceImage = document.getElementById("computer-choice");
+const playerChoiceImage = document.getElementById("your-choice-image");
+const computerChoiceImage = document.getElementById("computer-choice-image");
 let playerChoice = "";
 let computerChoice = "";
 const possibleComputerChoices = ["rock", "paper", "scissors", "lizard", "spock"];
@@ -18,7 +18,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Play Again!");
             } else {
                 let option = this.getAttribute("id");
-                alert(`You clicked ${option}`);
                 makeChoices(option);
             }
         });
@@ -29,9 +28,14 @@ function playGame() {}
 
 function makeChoices(option) {
     playerChoice = option;
+    playerChoiceImage.setAttribute('src', `assets/images/${playerChoice}.svg`);
 
     const randomChoice = Math.floor(Math.random() * 5);
     computerChoice = possibleComputerChoices[randomChoice];
+    computerChoiceImage.setAttribute('src', `assets/images/${computerChoice}.svg`);
+
+
+    checkWinner();
 }
 
 function checkWinner() {}
